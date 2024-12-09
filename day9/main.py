01c9a59273933ef5bf1992605232ef412f9fce6b
nikolay <alexandroff.nikolay05@gmail.com>
2024-12-09 17:19:28 +0000

    day9 checksum for single digits only

--- day9/main.py
+++ day9/main.py
@@ -4,33 +4,60 @@ def build(fname):
 
 
 def solve1(fname):
-    line = build(fname)
+    diskmap = build(fname)
     s = ""
     i = 0
     id = 0
-    for c in line:
+    for c in diskmap:
+        # even position in map means store c blocks at current id
         if i % 2 == 0:
             s += str(id) * int(c)
             id += 1
+        # odd position means c blank blocks
         else:
             s += "." * int(c)
         i += 1
 
-    max_id = id - 1
-    i = next(i for i, c in enumerate(s) if c == ".")
+    idr = id - 1
+    idl = 0  # left id, can infer right id from block size
+    print(s, "max_id", idr)
+    i = 0
     j = len(s)
+    c = 0
     while i < j:
-        block = s[j - len(str(max_id)) : j]
+        block = s[j - len(str(idr)) : j]  # get last len(idr) chars
         block_size = len(block)
-        if s[i : i + block_size] == "." * block_size:
-            s = s[:i] + block + s[i + block_size :]
-            s = s[: j - len(str(max_id))] + "." * (block_size + len(s[j:]))
-            while s[i] != ".":
-                i += 1
-            j -= block_size
-        print(s)
 
-    return 0
+        # move right pointer if cant grab a block
+        if "." in block:
+            j -= 1
+            continue
+
+        # move left pointer until block fits into space
+        if s[i : i + block_size] != "." * block_size:
+            while s[i] != ".":
+                checksum = int(i) * idl
+                print(f"{i} * {idl}={checksum}")
+                c += checksum
+                i += 1
+            idl += 1
+            continue
+
+        # copy right block into space on the left
+        s = s[:i] + block + s[i + block_size :]
+        s = s[: j - len(str(idr))] + "." * (block_size + len(s[j:]))
+        print(s, block_size, i, j)
+
+        # add copied block to the checksum
+        checksum = int(i) * int(block)
+        print(f"{i} * {block}={checksum}")
+        c += checksum
+        i += block_size
+        j -= block_size
+
+    assert s == "0099811188827773336446555566.............."
+
+    return c
 
 
 if __name__ == "__main__":
