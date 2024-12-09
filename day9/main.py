0fda19de1176f11360d7dae031af423f378d3008
nikolay <alexandroff.nikolay05@gmail.com>
2024-12-09 22:09:54 +0000

    slow no swe

--- day9/main.py
+++ day9/main.py
@@ -3,142 +3,64 @@ def build(fname):
         return f.read().strip()
 
 
-class Block:
-    def __init__(self, is_empty=False, size=None, id=None):
-        self.is_empty = is_empty
-        self.size = size
-        self.id = id
-
-    @staticmethod
-    def empty(size):
-        return Block(is_empty=True, id=None, size=size)
-
-    @staticmethod
-    def data(id):
-        return Block(is_empty=False, id=id, size=len(str(id)))
-
-    def __str__(self):
-        if self.is_empty and self.size is not None:
-            return "." * self.size
-        if self.id is None:
-            print(self.is_empty, self.size, self.id)
-            raise Exception("WTDDD")
-        return str(self.id)
+def from_map(diskmap: str):
+    slots = []
+    i = 0
+    id = 0
+    for c in diskmap:
+        if i % 2 == 0:  # File blocks
+            for _ in range(int(c)):
+                slots.append(id)
+            id += 1
+        else:  # Empty blocks
+            slots.extend([None] * int(c))
+        i += 1
+    return slots
 
 
-class Disk:
-    def __init__(self):
-        self.blocks = []
-
-    def add_block(self, block):
-        self.blocks.append(block)
-
-    @staticmethod
-    def from_map(diskmap: str):
-        i = 0
-        id = 0
-        disk = Disk()
-        for c in diskmap:
-            # even position in map means store c blocks at current id
-            if i % 2 == 0:
-                for _ in range(int(c)):
-                    disk.add_block(Block.data(id))
-                id += 1
-            # odd position means c blank blocks
-            else:
-                size = int(c)
-                if size:
-                    disk.add_block(Block.empty(size=size))
-            i += 1
-
-        return disk
-
-    def __str__(self):
-        return "".join([str(b) for b in self.blocks])
-
-    def __repr__(self):
-        return self.__str__()
-
-    def peek(self):
-        for i in range(len(self.blocks) - 1, -1, -1):
-            if not self.blocks[i].is_empty:
-                return self.blocks[i], i
-
-        return None, -1
-
-    def is_compact(self):
-        _, ix = self.peek()
-        for i in range(len(self.blocks)):
-            if i <= ix and self.blocks[i].is_empty:
-                return False
-            if i > ix and not self.blocks[i].is_empty:
-                return False
-        return True
-
-    def pop(self):
-        # remove the last non-empty block
-        for i in range(len(self.blocks) - 1, -1, -1):
-            if not self.blocks[i].is_empty:
-                el = self.blocks.pop(i)
-                self.blocks.insert(i, Block.empty(el.size))
-                return el
-
-    def insert(self):
-        """
-        Find an empty block whose size is greater or equal to this block id and insert it
-        """
-        block, ix = self.peek()
-        if not block:
-            raise Exception("No block to insert")
-
-        for i, b in enumerate(self.blocks):
-            if b.is_empty and b.size == block.size:
-                # replace the empty block with the new block
-                def cb():
-                    self.blocks[ix] = Block.empty(block.size)
-                    self.blocks[i] = block
-
-                return True, cb
-            elif b.is_empty and b.size > block.size:
-                # split the empty block into two
-                def cb():
-                    self.blocks[ix] = Block.empty(block.size)
-                    self.blocks[i] = block
-                    self.blocks.insert(i + 1, Block.empty(b.size - block.size))
-
-                return True, cb
-            else:
-                continue
-        return False, None
-
-    def checksum(self):
-        return sum(b.id * i for i, b in enumerate(self.blocks) if not b.is_empty)
-
-
-def solve1(fname, debug=False):
+def solve1(fname):
     diskmap = build(fname)
-    disk = Disk.from_map(diskmap)
+    slots = from_map(diskmap)
 
-    if debug:
-        assert str(disk) == "00...111...2...333.44.5555.6666.777.888899"
+    # Keep going until we can't find any moves to make
+    while True:
+        # Find rightmost filled slot
+        right_pos = -1
+        for i in range(len(slots) - 1, -1, -1):
+            if slots[i] is not None:
+                right_pos = i
+                break
+        if right_pos == -1:
+            break
 
-    print("compressing...")
-    while not disk.is_compact():
-        can, cb = disk.insert()
-        if can and cb:
-            cb()
-    print("finished")
+        # Find leftmost empty slot before right_pos
+        left_pos = -1
+        for i in range(right_pos):
+            if slots[i] is None:
+                left_pos = i
+                break
 
-    if debug:
-        assert str(disk) == "0099811188827773336446555566.............."
+        # If no empty slots found, we're done
+        if left_pos == -1:
+            break
 
-    return disk.checksum()
+        # Move the value left
+        slots[left_pos] = slots[right_pos]
+        slots[right_pos] = None
+
+    # Calculate checksum
+    return sum(i * id for i, id in enumerate(slots) if id is not None)
 
 
 if __name__ == "__main__":
-    p1_test = solve1("test.txt", debug=True)
+    p1_test = solve1("test.txt")
     print("p1 test:", p1_test)
     assert p1_test == 1928
 
+    import time
+
+    t = time.time()
     p1 = solve1("input.txt")
+    print("time:", time.time() - t)
     print("p1:", p1)
+    assert p1 == 6334655979668
