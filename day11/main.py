b3597e8600c640b6a4a2d5b50696a683cb4c98cc
nikolay <alexandroff.nikolay05@gmail.com>
2024-12-21 17:41:34 +0000

    solve 11

--- day11/main.py
+++ day11/main.py
@@ -7,48 +7,47 @@ def build(fname):
     return out
 
 
-def get_num_digits(n):
-    return len(str(n))
+def solve(fname, blinks):
+    # Map of number -> count of occurrences
+    counts = {}
+    for n in build(fname):
+        counts[n] = counts.get(n, 0) + 1
 
-
-def split_number(n, digits):
-    strstone = str(n)
-    ll = len(str(n)) // 2
-    return [int(strstone[:ll]), int(strstone[ll:])]
-
-
-def solve1(fname, blinks):
-    stones = build(fname)
-    c = 0
-
-    while c < blinks:
-        new_stones = []
-        for s in stones:
-            if s == 0:
-                new_stones.append(1)
+    for _ in range(blinks):
+        new_counts = {}
+        for num, freq in counts.items():
+            if num == 0:
+                new_counts[1] = new_counts.get(1, 0) + freq
             else:
-                if (digits := get_num_digits(s)) % 2 == 0:
-                    new_stones.extend(split_number(s, digits))
+                s = str(num)
+                if len(s) % 2 == 0:
+                    # Split numbers in half
+                    mid = len(s) // 2
+                    left = int(s[:mid])
+                    right = int(s[mid:])
+                    new_counts[left] = new_counts.get(left, 0) + freq
+                    new_counts[right] = new_counts.get(right, 0) + freq
                 else:
-                    new_stones.append(s * 2024)
+                    # Multiply by 2024
+                    new_val = num * 2024
+                    new_counts[new_val] = new_counts.get(new_val, 0) + freq
+        counts = new_counts
 
-        stones = new_stones
-        c += 1
-
-    return len(stones)
+    return sum(counts.values())
 
 
 if __name__ == "__main__":
-    t1 = solve1("test.txt", 1)
+    t1 = solve("test.txt", 1)
     print(t1)
     assert t1 == 7
 
-    t2 = solve1("test2.txt", 25)
+    t2 = solve("test2.txt", 25)
     print(t2)
     assert t2 == 55312
 
-    p1 = solve1("input.txt", 25)
+    p1 = solve("input.txt", 25)
     print(p1)
+    assert p1 == 194482
 
-    # p2 = solve1("input.txt", 75)
-    # print(p2)
+    p2 = solve("input.txt", 75)
+    print(p2)
